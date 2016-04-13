c71200bfd58f1d7be98c6c3d015bb873d3fce3df
Davin Suen <y6suen@gmail.com>
2016-04-13 02:21:54 -0400

    js on nav

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,9 +1,33 @@
+// Manipulate navigation based on active section
+var sectionScroll = function() {
+	$(window).on('scroll', function() {
+
+		// Measure current position from top of page
+		var currentPosition = $(this).scrollTop();
+
+		// Measure top of each section's distance from top of page
+		$('section').each(function() {
+			var sectionPosition = $(this).offset().top;
+			var sectionId = $(this).attr('id');
+
+			// If current position is equal or greater than (aka further down) section's top, add active class
+			if (currentPosition >= sectionPosition ) {
+				$('nav a').removeClass('active');
+				$('nav a[href=#' + sectionId + ']').addClass('active');
+			}
+		});
+
+	});
+} // sectionScroll
+
 $(function() {
+
+	sectionScroll();
 
 	$('nav a').on('click', function() {
 		$('nav a').removeClass('active');
 		$(this).addClass('active');
-		console.log('click');
+		$(this).smoothScroll();
 	});
 
 });
