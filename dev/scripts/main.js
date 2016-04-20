36a4a4154e02b27c5cfb15d81f025159aad96a0a
Davin Suen <y6suen@gmail.com>
2016-04-19 23:36:32 -0400

    responsive & javascript

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,35 +1,133 @@
-// Manipulate navigation based on active section
-var sectionScroll = function() {
+/* ====================
+	GLOBAL APP
+======================= */
+var app = {};
+
+/* ====================
+	GLOBAL OBJECTS
+======================= */
+var activeNav = {}; // Nav animation for active section
+var highlightNav = {} // Change the nav's colour for some sections
+var smoothScroll = {}; // SmoothScroll
+
+// Create array of nav items
+var aNavArray = $('header a');
+// Creat empty array to store href
+var hrefNavArray = [];
+
+// For each nav item, get href and store in hrefNavArray
+for (var i = 0; i < aNavArray.length; i++) {
+	// Get href
+	activeNav.href = $(aNavArray[i]).attr('href');
+	// Push to new array
+	hrefNavArray.push(activeNav.href);
+}
+// console.log(hrefNavArray);
+
+/* ====================
+	activeNav.animate
+======================= */
+activeNav.animate = function() {
+
 	$(window).on('scroll', function() {
 
-		// Measure current position from top of page
-		var currentPosition = $(this).scrollTop();
+		// get offset of the window from top of page
+		activeNav.windowPos = $(this).scrollTop();
+		// get height of window
+		activeNav.windowHeight = $(this).height();
+		// get height of entire document page
+		activeNav.docHeight = $(document).height(); 
 
-		// Measure top of each section's distance from top of page
-		$('section').each(function() {
-			var sectionPosition = $(this).offset().top;
-			sectionPosition -= 200;
-			var sectionId = $(this).attr('id');
+		// Add active class to nav item when section with the same ID is at top of page
+		for (var i = 0; i < hrefNavArray.length; i++) {
+			var sectionId = hrefNavArray[i];
+			// get offset of each section from top of page
+			activeNav.sectionPos = $(sectionId).offset().top;
+			activeNav.sectionPos -= 200;
+			// get height of each section
+			activeNav.sectionHeight = $(sectionId).height();
 
-			// If current position is equal or greater than (aka further down) section's top, add active class
-			if (currentPosition >= sectionPosition ) {
-				$('nav a').removeClass('active');
-				$('nav a[href=#' + sectionId + ']').addClass('active');
-			}
-			
+			// See if top of window is within top and bottom range of section
+			if (activeNav.windowPos >= activeNav.sectionPos && activeNav.windowPos < (activeNav.sectionPos + activeNav.sectionHeight)) {
+				$('a[href="' + sectionId + '"]').addClass('active');
+			} 
+			else {
+				$('a[href="' + sectionId + '"]').removeClass('active');
+			} // if
+
+		}; // for
+
+		// If offset of window from top of page is greater than or equal to offset of skills section from top of page then add class headerLight
+		if (activeNav.windowPos >= $('#skills').offset().top) {
+			$('nav , header .logo').addClass('headerLight');
+		}
+		else {
+			$('nav , header .logo').removeClass('headerLight');
+		} // if
+
+	}); // on scroll
+
+} // activeNav.animate
+
+
+/* ====================
+	activeNav.showLogo
+======================= */
+activeNav.showLogo = function() {
+
+	activeNav.windowWidth = window.innerWidth;
+
+	if (activeNav.windowWidth <= 600) {
+		$('header .logo').show();
+	}
+	else {
+		if(activeNav.windowPos >= $('#about').offset().top) {
+			$('header .logo').fadeIn();
+		} else {
+			$('header .logo').fadeOut();
+		}
+	}
+
+} // activeNav.showLogo
+
+
+/* ====================
+	smoothScroll.smooth
+======================= */
+smoothScroll.smooth = function() {
+
+	smoothScroll.windowWidth = window.innerWidth;
+
+	if (smoothScroll.windowWidth <= 600) {
+		$('header a').smoothScroll({
+			offset: -70
 		});
+	} else {
+		$('header a').smoothScroll({
+			offset: 0
+		});
+	}
 
+} //smoothScroll.smooth
+
+
+/* ====================
+	APP INIT
+======================= */
+app.init = function() {
+	activeNav.animate();
+	activeNav.showLogo();
+	smoothScroll.smooth();
+	$(window).on('resize', function() {
+		smoothScroll.smooth();
+		activeNav.showLogo();
 	});
-} // sectionScroll
+	$(window).on('scroll',function() {
+		activeNav.showLogo();
+	});
+}
+
 
 $(function() {
-
-	sectionScroll();
-	$('nav a').smoothScroll();
-
-	$('nav a').on('click', function() {
-		$('nav a').removeClass('active');
-		$(this).addClass('active');
-	});
-
+		app.init();
 });
